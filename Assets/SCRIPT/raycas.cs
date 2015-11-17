222b768eafa16fffd35948b2433da5ff69c58a1b
aran <aas711@nyu.edu>
2015-11-17 12:42:08 -0800

    trash finalproj is the test scene

---
+++ Assets/SCRIPT/raycas.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class raycas : MonoBehaviour {
+	Camera cam;
+	public Text textUI;
+	public Text textUI2;
+	public Transform player;
+	public Transform cup;
+	public GameObject mhm;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			Ray ray = cam.ScreenPointToRay(new Vector3(400, 400, 0));
+		Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
+		RaycastHit RayHitInfo = new RaycastHit();
+		if ( Physics.Raycast (ray, out RayHitInfo, 100f ) ) {
+			if (RayHitInfo.collider.tag == "cup"){
+				Debug.Log ("CUP");
+			
+				mhm.SetActive(true);
+				if (!GameManager.instance.zoom){
+					textUI.text = "(T) CUP";
+					textUI2.text = "(T) CUP";
+				
+				if (Input.GetKeyDown (KeyCode.T)){
+					Camera.main.fieldOfView = 40;
+					GameManager.instance.zoom= true;
+					textUI.text = "? TOPPLE \n ? DRINK";
+					textUI2.text = "? TOPPLE \n ? DRINK";
+					}}
+
+		}
+		
+		}
+	}
+
+}
---
+++ Assets/SCRIPT/rigibidoe.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rigibidoe : MonoBehaviour {
+   
+
+    // Use this for initialization
+  
+
+
+   
+    void Update()
+    {
+   
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(new Vector3(0f, 0f, 25f * Time.deltaTime)); 
+			
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (new Vector3(0f, 0f, -25f * Time.deltaTime)); 
+			
+		}
+
+		if (!GameManager.instance.zoom){
+
+			if (Input.GetKey (KeyCode.A)) {
+				transform.localEulerAngles += new Vector3(0f, -2f, 0f);
+				
+			}
+			if (Input.GetKey (KeyCode.D)) {
+				transform.localEulerAngles += new Vector3(0f, 2f, 0f); 
+				
+			}     
+			if (Input.GetKey (KeyCode.Q)){
+				if (Camera.main.transform.eulerAngles.x >0f && Camera.main.transform.eulerAngles.x <80f){
+					Camera.main.transform.eulerAngles += new Vector3 (15f,0f,0f) * Time.deltaTime;
+				}
+			}
+			if (Input.GetKey (KeyCode.E)){
+				if (Camera.main.transform.eulerAngles.x >0f && Camera.main.transform.eulerAngles.x <80f){
+					Camera.main.transform.eulerAngles += new Vector3 (-15f,0f,0f) * Time.deltaTime;
+				}
+    }
+
+	}
+
+
+	}}
