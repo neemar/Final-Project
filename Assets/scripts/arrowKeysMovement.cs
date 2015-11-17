b8c4a49380fed05e06ed91b1a0256df9ade2d44e
Lucy Ungaro <lucyu12@gmail.com>
2015-11-16 22:05:14 -0500

    clap like you odn't care

---
+++ Assets/scripts/arrowKeysMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arrowKeysMovement : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate(new Vector3(0f, 0f, 10f * Time.deltaTime)); 
+			
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (new Vector3(0f, 0f, -10f * Time.deltaTime)); 
+			
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.localEulerAngles += new Vector3(0f, -15f, 0f);
+			
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.localEulerAngles += new Vector3(0f, 15f, 0f); 
+			
+		}
+	}
+}
