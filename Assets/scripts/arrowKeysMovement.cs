876ee9fa96032b9babd7dceb4028185568f8060a
Lucy Ungaro <lucyu12@gmail.com>
2015-11-24 14:54:44 -0500

    interaction with scissors and cord, scissors and tv models

--- Assets/scripts/arrowKeysMovement.cs
+++ Assets/scripts/arrowKeysMovement.cs
@@ -2,23 +2,38 @@
 using System.Collections;
 
 public class arrowKeysMovement : MonoBehaviour {
+	Rigidbody rbody; 
+
+	void Start (){
+		rbody = GetComponent<Rigidbody> (); 
+	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.UpArrow)) {
+			//rbody.AddForce(new Vector3(0f, 0f, 10f * Time.deltaTime)); 
 			transform.Translate(new Vector3(0f, 0f, 10f * Time.deltaTime)); 
+			Debug.Log ("I am moving to the right!"); 
+
 			
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
-			transform.Translate (new Vector3(0f, 0f, -10f * Time.deltaTime)); 
+			//rbody.AddForce(new Vector3(0f, 0f, -10f * Time.deltaTime)); 
+			transform.Translate(new Vector3(0f, 0f, -10f * Time.deltaTime)); 
+
+			Debug.Log ("I am moving to the right!"); 
+
 			
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.localEulerAngles += new Vector3(0f, -10f, 0f);
+			Debug.Log ("I am moving to the right!"); 
+
 			
 		}
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.localEulerAngles += new Vector3(0f, 10f, 0f); 
+			Debug.Log ("I am moving to the right!"); 
 			
 		}
 
