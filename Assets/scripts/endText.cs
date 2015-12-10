f2765840a06a4eff573e669dce2818000bc89a52
Lucy Ungaro <lucyu12@gmail.com>
2015-12-10 16:32:30 -0500

    working scene

--- Assets/scripts/endText.cs
+++ Assets/scripts/endText.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI; 
 public class endText : MonoBehaviour {
-	Text myButts; 
+	public Text myButts; 
 	// Use this for initialization
 	void Start () {
 	
@@ -10,9 +10,9 @@ public class endText : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (ScoreManager.playerOneScore >= 10) {
+		if (ScoreManager.playerOneScore >= 3) {
 			myButts.text = "PLAYER ONE WINS!"; 
-		} else if (ScoreManager.playerTwoScore >= 10) {
+		} else if (ScoreManager.playerTwoScore >= 3) {
 			myButts.text = "PLAYER TWO WINS!"; 
 		}
 	
