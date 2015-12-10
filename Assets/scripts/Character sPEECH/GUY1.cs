869e2375cda0b8e137b204a12d6eea88d985198c
aran <aas711@nyu.edu>
2015-12-10 14:14:50 -0500

    scene is dec10ug elevator scene updated but unfinished

--- Assets/scripts/Character sPEECH/GUY1.cs
+++ Assets/scripts/Character sPEECH/GUY1.cs
@@ -4,59 +4,230 @@ using UnityEngine.UI;
 
 public class GUY1 : MonoBehaviour {
 
-	public Transform leftperson;
-	public Transform rightperson;
-	public Text leftpersontext;
-	public Text rightpersontext;
+	public Transform person;
+	public Transform person1;
+	public Transform person2;
+	public Transform person3;
+	public Transform person31;
+	public Transform person4;
+	public Transform person41;
+	public Transform person5;
+	public Transform person51;
+	public Transform person6;
+	public Transform person61;
+	
+	public Text chartext;
+
+	public bool notplaying= true;
+	public bool notplaying1= true;
+	public bool notplaying2= true;
+	public bool notplaying3= true;
+	public bool notplaying4= true;
+	public bool notplaying5= true;
+	public bool notplaying6= true;
+	public bool secondperson= false;
+
+
+	string myString11 = ("Yes, I have devoted my life to culture!");
+	string myString10 = ("Culture...");
+		string myString9 = ("Of course, of course!");
+	string myString8 = ("Rockefeller, of course.");
+		string myString7 = ("John?");
+	string myString6 = ("The angle to the Chrysler Building is 3 degrees less stimulating than the view from John's place");
+		string myString5 = ("What a splendid view!");
+	string myString4 = ("Yeahhhh, I don’t subscribe to sweaters anymore.");
+	string myString3 = ("Hey did you see Kanye’s sweater in his last fashion show? it was ridiculous.");
 	string myString = ("I will never love again !");
-	string myString2 = ("The dead skyscrapers represent my heart");
+	string myString2 = ("The skyline represents my heart");
 	
 	string stringAnimate = ""; 
 
 
+
+
+	IEnumerator AnimateText5(){
+		for (int i = 0; i < myString5.Length; i++) {
+			stringAnimate += myString5[i]; 
+			yield return new WaitForSeconds(0.05f); 
+		}
+	}
+	IEnumerator AnimateText55(){
+		for (int i = 0; i < myString6.Length; i++) {
+			stringAnimate += myString6[i]; 
+			yield return new WaitForSeconds(0.05f); 
+		}
+	}
+	IEnumerator AnimateText555(){
+		for (int i = 0; i < myString7.Length; i++) {
+			stringAnimate += myString7[i]; 
+			yield return new WaitForSeconds(0.05f); 
+		}
+	}
+	IEnumerator AnimateText5555(){
+		for (int i = 0; i < myString8.Length; i++) {
+			stringAnimate += myString8[i]; 
+			yield return new WaitForSeconds(0.05f); 
+		}
+	}
+	IEnumerator AnimateText55555(){
+		for (int i = 0; i < myString9.Length; i++) {
+			stringAnimate += myString9[i]; 
+			yield return new WaitForSeconds(0.05f); 
+		}
+	}
+	IEnumerator AnimateText555555555555(){
+	while (true){
+		stringAnimate = "";
+		yield return StartCoroutine(AnimateText5());
+		
+		yield return new WaitForSeconds(1f); 
+		
+		stringAnimate = "";
+		yield return StartCoroutine(AnimateText55());
+		
+		yield return new WaitForSeconds(1f); 
+		stringAnimate = "";
+			yield return StartCoroutine(AnimateText555());
+			
+			yield return new WaitForSeconds(1f); 
+			stringAnimate = "";
+			yield return StartCoroutine(AnimateText5555());
+			
+			yield return new WaitForSeconds(1f); 
+			stringAnimate = "";
+			yield return StartCoroutine(AnimateText55555());
+			
+			yield return new WaitForSeconds(1.5f); 
+			stringAnimate = "";
+	}}
+
+
+IEnumerator AnimateText4(){
+		for (int i = 0; i < myString3.Length; i++) {
+			stringAnimate += myString3[i]; 
+			yield return new WaitForSeconds(0.05f); 
+		}
 	
-	IEnumerator AnimateText(){
-		for (int i = 0; i < myString.Length; i++) {
-			stringAnimate += myString[i]; 
-			yield return new WaitForSeconds(0.07f); 
+
+
+	}	IEnumerator AnimateText44(){
+		for (int i = 0; i < myString3.Length; i++) {
+			stringAnimate += myString3[i]; 
+			yield return new WaitForSeconds(0.05f); 
+		}
+		
+	}
+
+
+
+	IEnumerator AnimateText4444444(){
+		while (true){
+			stringAnimate = "";
+			yield return StartCoroutine(AnimateText4());
+			
+			yield return new WaitForSeconds(1f); 
+			
+			stringAnimate = "";
+			yield return StartCoroutine(AnimateText44());
+			
+			yield return new WaitForSeconds(1f); 
+			stringAnimate = "";
+			
+		}}
+
+	IEnumerator AnimateText3(){
+		for (int i = 0; i < myString10.Length; i++) {
+			stringAnimate += myString10[i]; 
+			yield return new WaitForSeconds(0.05f); 
+		}
+		
+		
+		
+	}	IEnumerator AnimateText33(){
+		for (int i = 0; i < myString11.Length; i++) {
+			stringAnimate += myString11[i]; 
+			yield return new WaitForSeconds(0.05f); 
 		}
 		
 	}
 	
-	IEnumerator AnimateText2(){
+	
+	
+	IEnumerator AnimateText3333(){
+		while (true){
+			stringAnimate = "";
+			yield return StartCoroutine(AnimateText3());
+			
+			yield return new WaitForSeconds(1f); 
+			
+			stringAnimate = "";
+			yield return StartCoroutine(AnimateText33());
+			
+			yield return new WaitForSeconds(1f); 
+			stringAnimate = "";
+			
+		}}
+	
+	IEnumerator AnimateText1(){
+		for (int i = 0; i < myString.Length; i++) {
+			stringAnimate += myString[i]; 
+			yield return new WaitForSeconds(0.05f); 
+		}
+		
+	}
+	
+	IEnumerator AnimateText11(){
 		for (int i = 0; i < myString2.Length; i++) {
 			stringAnimate += myString2[i]; 
-			yield return new WaitForSeconds(0.07f); 
+			yield return new WaitForSeconds(0.05f); 
 		}
 	}
 	
-	IEnumerator AnimateText3(){
+	IEnumerator AnimateText111(){
 		while (true){
-			yield return StartCoroutine(AnimateText());
-			
-			yield return new WaitForSeconds(2f); 
-			
 			stringAnimate = "";
-			yield return StartCoroutine(AnimateText2());
+			yield return StartCoroutine(AnimateText1());
 			
-			yield return new WaitForSeconds(2f); 
+			yield return new WaitForSeconds(1f); 
+
 			stringAnimate = "";
+			yield return StartCoroutine(AnimateText11());
+			
+			yield return new WaitForSeconds(1f); 
+			stringAnimate = "";
+
 		}}
 	
 
 	void Update () {
+		float ewew1= Vector3.Distance(transform.position,person1.position);
+		float ewew3= Vector3.Distance(transform.position,person3.position);
+		chartext.text= (stringAnimate);
+		if (ewew1<60f && notplaying1) {
+
+			StartCoroutine( AnimateText111());
+			notplaying1 = false;
+			if (ewew1>60f) {
+				notplaying1=true;
+				StopAllCoroutines();
+				stringAnimate = "";
+			}
+		}
+
+		if (ewew3<60f && notplaying1) {
+			
+			StartCoroutine( AnimateText3333());
+			notplaying1 = false;
+			if (ewew3>60f) {
+				notplaying1=true;
+				StopAllCoroutines();
+				stringAnimate = "";
+			}
+		}
 
 
-		if (Vector3.Distance(leftperson.position,transform.position)<20f) {
-			leftpersontext.text= (stringAnimate);
-			StartCoroutine( AnimateText3());
-		}
-		
-		if (Vector3.Distance(rightperson.position,transform.position)<20f) {
-			rightpersontext.text= (stringAnimate);
-			StartCoroutine( AnimateText3());
-		}
-		
+
+
 	}
 	
 	
