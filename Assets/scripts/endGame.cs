449a7451258ca393cdd93ac2aa92c942cd432633
Lucy Ungaro <lucyu12@gmail.com>
2015-12-10 14:24:43 -0500

    hat, glasses, end scene

---
+++ Assets/scripts/endGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class endGame : MonoBehaviour {
+	 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ScoreManager.playerOneScore >= 10 || ScoreManager.playerTwoScore >= 10){
+			//load new level immediately or have some kind of transition?
+			Application.LoadLevel ("endGame"); 
+
+
+
+		
+
+		}
+	
+	}
+}
---
+++ Assets/scripts/endText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+public class endText : MonoBehaviour {
+	Text myButts; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ScoreManager.playerOneScore >= 10) {
+			myButts.text = "PLAYER ONE WINS!"; 
+		} else if (ScoreManager.playerTwoScore >= 10) {
+			myButts.text = "PLAYER TWO WINS!"; 
+		}
+	
+	}
+}
