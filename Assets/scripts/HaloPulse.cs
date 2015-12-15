0f7d6595a868ba5ee0b06cb3ffe52059300e8fdf
Rameen Shafiee <rs4047@nyu.edu>
2015-12-15 13:43:48 -0500

    12/15 commit 2

--- Assets/scripts/HaloPulse.cs
+++ Assets/scripts/HaloPulse.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class HaloPulse : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
-	
+//		this.GetComponent("Halo").size = size;
 	}
 	
 	// Update is called once per frame
